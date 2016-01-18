0cf7fa967be479bffbd081e2138785abf624a953
reactojango <yevhavencoder@gmail.com>
2016-01-18 07:01:15 -0500

    Fix: disable socket.io (refs: DPIM-1)

--- config/lib/express.js
+++ config/lib/express.js
@@ -271,7 +271,7 @@ module.exports.init = function (db) {
   this.initSwaggerMiddleware(app);
 
   // Configure Socket.io
-  app = this.configureSocketIO(app, db);
+  // app = this.configureSocketIO(app, db);
 
   return app;
 };
