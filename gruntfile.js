77d782ad56afd8cb4e9d8b620263f9da5f47e9ac
reactojango <yevhavencoder@gmail.com>
2016-11-11 15:58:08 +0200

    Fix: add swagger-doc to build task

--- gruntfile.js
+++ gruntfile.js
@@ -314,7 +314,7 @@ module.exports = function (grunt) {
   grunt.registerTask('lint', ['sass', 'less', 'jshint', 'eslint', 'csslint']);
 
   // Lint project files and minify them into two production files.
-  grunt.registerTask('build', ['env:dev', 'lint', 'ngAnnotate', 'uglify', 'cssmin']);
+  grunt.registerTask('build', ['env:dev', 'lint', 'ngAnnotate', 'uglify', 'cssmin', 'swagger-doc']);
 
   // Run the project tests
   grunt.registerTask('test', ['env:test', 'lint', 'mkdir:upload', 'copy:localConfig', 'server', 'mochaTest', 'karma:unit', 'protractor']);
