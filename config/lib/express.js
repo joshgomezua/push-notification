73e904af6bce8540402a46f1182e27451b088c13
reactojango <yevhavencoder@gmail.com>
2016-11-11 19:53:34 +0200

    Fix: allow cors methods

--- config/lib/express.js
+++ config/lib/express.js
@@ -220,7 +220,7 @@ module.exports.initCORS = function(app) {
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token, Authorization');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
   });
 };
