a8c98ff81efc8e5cf5c5048802abb7c3902946ef
reactojango <yevhavencoder@gmail.com>
2016-12-09 16:19:57 +0200

    Fix: parse filter lib date validation

--- modules/analytics/server/libs/filter.server.lib.js
+++ modules/analytics/server/libs/filter.server.lib.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 exports.parseFilter = function(filter) {
   var str = JSON.stringify(filter);
   var obj = JSON.parse(str, function(key, value) {
-    if (typeof value === 'string' && (moment(value, 'YYYY-MM-DD', true) || moment(value, 'YYYY-MM-DD HH:mm:ss', true))) {
+    if (typeof value === 'string' && (moment(value, 'YYYY-MM-DD', true).isValid() || moment(value, 'YYYY-MM-DD HH:mm:ss', true).isValid())) {
       return moment(value).toDate();
     }
 
