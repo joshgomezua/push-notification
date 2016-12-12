1b00cc224ef5cc7e167b932fad2b09924fc2a5bd
reactojango <yevhavencoder@gmail.com>
2016-12-12 06:24:37 +0200

    Fix: allow browser dependent gifs

--- modules/core/server/libs/images.server.lib.js
+++ modules/core/server/libs/images.server.lib.js
@@ -18,39 +18,47 @@ exports.uploadToAWS = function(imgObj, cb, staticImg) {
     secretAccessKey: config.aws.secretAccessKey
   });
   // For now let's accept only gif files and no resizing
-  if ((gifInfo.valid && gifInfo.duration && !staticImg) || staticImg) {
-    // upload to amazon
-    var s3Bucket = new AWS.S3({
-      params: {
-        Bucket: config.aws.bucket
-      }
-    });
-    var params = {
-      Body: fs.createReadStream(imgObj.path),
-      ACL: 'public-read',
-      ContentType: imgObj.mimetype,
-      Key: 'campaigns/' + imgObj.filename
-    };
-    s3Bucket.putObject(params, function(err, data){
-      if (!staticImg) {
-        var imageJson = {
-          url: 'https://s3.amazonaws.com/' + config.aws.bucket + '/' + params.Key,
-          type: imgObj.mimetype,
-          size: imgObj.size,
-          duration: gifInfo.duration || 0
-        };
-        var image = new ImageModel(imageJson);
-        image.save(function (err) {
-          if (err) {
-            cb(err, null);
-          } else {
-            cb(null, image);
-          }
-        });
-      } else {
-        cb(null, 'https://s3.amazonaws.com/' + config.aws.bucket + '/' + params.Key);
-      }
-    });
+  if ((gifInfo.valid && !staticImg) || staticImg) {
+    var duration = gifInfo.duration;
+    if (!duration && gifInfo.isBrowserDuration && gifInfo.durationChrome) {
+      duration = gifInfo.durationChrome;
+    }
+    if (!duration && !staticImg) {
+      cb('Can not get duration of the gif.', null);
+    } else {
+      // upload to amazon
+      var s3Bucket = new AWS.S3({
+        params: {
+          Bucket: config.aws.bucket
+        }
+      });
+      var params = {
+        Body: fs.createReadStream(imgObj.path),
+        ACL: 'public-read',
+        ContentType: imgObj.mimetype,
+        Key: 'campaigns/' + imgObj.filename
+      };
+      s3Bucket.putObject(params, function(err, data){
+        if (!staticImg) {
+          var imageJson = {
+            url: 'https://s3.amazonaws.com/' + config.aws.bucket + '/' + params.Key,
+            type: imgObj.mimetype,
+            size: imgObj.size,
+            duration: duration || 0
+          };
+          var image = new ImageModel(imageJson);
+          image.save(function (err) {
+            if (err) {
+              cb(err, null);
+            } else {
+              cb(null, image);
+            }
+          });
+        } else {
+          cb(null, 'https://s3.amazonaws.com/' + config.aws.bucket + '/' + params.Key);
+        }
+      });
+    }
   } else {
     cb('Image is not valid gif.', null);
   }
